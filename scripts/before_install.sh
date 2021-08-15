aa81d0c7071d101c4ab4bf6e12799f11a694f492
Jorge Hernandez <jorgehc03@gmail.com>
2021-08-15 03:52:59 -0400

    Demo

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,1 +1,5 @@
 rm -f /var/www/html/*
+sudo mkdir /var/www/your_domain
+sudo chown -R $USER:$USER /var/www/your_domain
+sudo chmod -R 755 /var/www/your_domain
+nano /var/www/your_domain/index.html
---
+++ scripts/start_server.sh
@@ -0,0 +1,3 @@
+sudo a2ensite your_domain.conf
+sudo a2dissite 000-default.conf
+sudo systemctl restart apache2
